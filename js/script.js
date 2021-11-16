a9f89ea4dfc542167b83cc46a54940767338ae22
indopanda <41268815+indopanda@users.noreply.github.com>
2021-11-15 20:24:15 -0500

    11/15 8:23 PM
    Attempting extra credit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-//Start at step 8.
-
 /******************************************
 Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
@@ -8,6 +6,20 @@ project 1 - A Random Quote Generator
 // For assistance: 
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+function backgroundColor () {
+  let red;
+  let green;
+  let blue;
+  let randomRGB;
+
+  red = Math.floor(Math.random() * 256);
+  green = Math.floor(Math.random() * 256);
+  blue = Math.floor(Math.random() * 256);
+  randomRGB = `background color: rgb(${red}, ${green}, ${blue})`;
+  console.log(randomRGB);
+  document.body.style.backgroundColor = randomRGB;
+}
 
 /*** 
  * `quotes` array 
@@ -18,45 +30,49 @@ const quotes = [
     quote: "If you're comfortable while you're doing it, probably doing it wrong.",
     source: "Ted Lasso",
     citation: "Ted Lasso",
-    year: 2020
-    //add tags:
+    year: 2020,
+    tags: "Sports"
   },
   {
     quote: "I'm good at everything, except the things I can't do.",
     source: "Mi-Nyeo",
     citation: "Squid Game",
-    year: 2021
+    year: 2021,
+    tags: "Drama"
   },
   {
     quote: "He may have been your father, boy, but he wasn't your daddy.",
     source: "Yondu Udonta",
-    citation: "Guardians of the Galaxy Vol. 2",
-    year: 2017
+    citation: /*"Guardians of the Galaxy Vol. 2"*/'',
+    year: 2017,
+    tags: "Action/Sci-fi"
   },
   {
     quote: "Don’t have a good day- have a GREAT day!",
-    source: "Guy",
+    source: "Guy"/*,
     citation: "Free Guy",
-    year: 2021
+    year: 2021,
+    tags: "Action/Adventure"*/
   },
   {
     quote: "Beneath this mask there is more than flesh. Beneath this mask there is an idea. And ideas are are bulletproof.",
-    source: "V",
+    source: "V"/*,
     citation: "V for Vendetta",
-    year: 2005
+    year: 2005,
+    tags: "Action/Thriller"*/
   }
 ];
 
-console.log(quotes);
+//console.log(quotes);
 
 /***
  * `getRandomQuote` function
 ***/
-
 function getRandomQuote() {
   let randomNum = Math.floor(Math.random() * quotes.length);
-  //return quotes[randomNum];
-  return randomNum;
+  //returns quote object
+  return quotes[randomNum];
+  //return randomNum;
 }
 //console.log(getRandomQuote());
 
@@ -64,21 +80,42 @@ function getRandomQuote() {
 /***
  * `printQuote` function
 ***/
-
-
 function printQuote() {
-  //1. call getRandomQuote
   const num = getRandomQuote();
-  //2. use the returned quote to build a string of HTML
-  const randomQuote = `<p class="quote">${quotes[num].quote}</p>
+  console.log(num);
+
+  /*const randomQuote = `<p class="quote">${quotes[num].quote}</p>
                       <p class="source">${quotes[num].source}
                         <span class="citation">${quotes[num].citation}</span>
                         <span class="year">${quotes[num].year}</span>
-                      </p>`;
-  //3. display that string to display a random quote in the browser
-console.log(randomQuote);
+                      </p>`;*/
+
+  let randomQuote = `<p class="quote">${num.quote}</p>
+  <p class="source">${num.source}`;
+
+  //concatenates citation if object has citation property 
+  if (num.citation) {
+    console.log(num.citation);
+    randomQuote += `<span class="citation">${num.citation}</span>`;
+  }
+
+  //concatenates year if object has year property 
+  if (num.year) {
+    console.log(num.year);
+    randomQuote += `<span class="year">${num.year}</span>`;
+  }
+
+  //concatenates tag if object has tag property 
+  if (num.tags) {
+    randomQuote += `<span class="year">${num.tags}</span>`;
+  }
+
+  //closes quote
+  randomQuote += `</p>`;
+
+//console.log(randomQuote);
+document.getElementById('quote-box').innerHTML = randomQuote ; 
 }
-printQuote();
 
 
 /***
@@ -87,3 +124,6 @@ printQuote();
 ***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document.getElementById('load-quote').addEventListener("click", backgroundColor);
+
+
